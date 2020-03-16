d0089c0340d2e85f3ca88c4b7676a0e695af62a9
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-03-16 01:06:19 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,13 +15,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-contacts.each do |person, data|
-    data.each do |attr, value|
-      if attr == :favorite_icecream_flavors
-        value.delete_if { |flavor| flavor == "strawberry" }
+contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
       end
     end
   end
-end
-contacts
 end
